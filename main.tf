8dbe8d64b4747aeb77f9b779b5488631fdc93df7
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-06 15:16:16 -0400

    Fix reference to key name

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "tfe_prereqs_w2" {
   # --- Bastion --- #
   create_bastion                 = true
   bastion_instance_type          = "t3a.small"
-  bastion_ec2_keypair_name       = "terraform-key"
+  bastion_ec2_keypair_name       = "acme-w2"
   bastion_cidr_allow_ingress_ssh = ["69.53.107.107/32"]
 
   # --- TLS certificates --- #
