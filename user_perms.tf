8a484c4bfae6128f770cf0b33cef7e12f9a2ddc1
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-05-13 16:07:33 -0400

    And for the friendly human as well...

---
+++ user_perms.tf
@@ -0,0 +1,19 @@
+resource "aws_eks_access_entry" "tfe_cluster_human" {
+  cluster_name  = module.tfe.eks_cluster_name
+  principal_arn = local.it_me
+  type          = "STANDARD"
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_access_policy_association" "tfe_cluster_human" {
+  access_scope {
+    type       = "cluster"
+    namespaces = []
+  }
+
+  cluster_name = module.tfe.eks_cluster_name
+
+  policy_arn    = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = local.it_me
+}
