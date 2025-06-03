b4a112fc40c1428b0914418c24fadc532471af97
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-06-03 16:13:28 -0400

    more perms

--- user_perms.tf
+++ user_perms.tf
@@ -18,6 +18,14 @@ resource "aws_eks_access_policy_association" "tfe_cluster_human" {
   principal_arn = local.it_me
 }
 
+resource "aws_eks_access_entry" "tfe_pi_cluster_human" {
+  cluster_name  = module.tfe_pi.eks_cluster_name
+  principal_arn = local.it_me
+  type          = "STANDARD"
+
+  tags = local.common_tags
+}
+
 resource "aws_eks_access_policy_association" "tfe_pi_cluster_human" {
   access_scope {
     type       = "cluster"
