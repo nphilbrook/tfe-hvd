62d0d70fec3627444ffbea9a29474770d56e6928
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-06-04 14:22:42 -0400

    Create r53 record for PI TFE

--- r53.tf
+++ r53.tf
@@ -2,10 +2,30 @@ data "aws_route53_zone" "public_zone" {
   name = local.r53_zone
 }
 
+data "aws_lb" "primary" {
+  tags = {
+    "elbv2.k8s.aws/cluster" = "primary-tfe-eks-cluster"
+  }
+}
+
+data "aws_lb" "pi" {
+  tags = {
+    "elbv2.k8s.aws/cluster" = "pi-tfe-eks-cluster"
+  }
+}
+
 resource "aws_route53_record" "tfe" {
   zone_id = data.aws_route53_zone.public_zone.zone_id
   name    = local.tfe_fqdn
   type    = "CNAME"
   ttl     = 300
-  records = ["k8s-tfe-terrafor-04ceae181f-acbb33607c5d8af0.elb.us-west-2.amazonaws.com"]
+  records = [data.aws_lb.primary.dns_name]
+}
+
+resource "aws_route53_record" "tfe_pi" {
+  zone_id = data.aws_route53_zone.public_zone.zone_id
+  name    = local.tfe_pi_fqdn
+  type    = "CNAME"
+  ttl     = 300
+  records = [data.aws_lb.pi.dns_name]
 }
