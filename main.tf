35fd2d8ef85d54821044d2b9b0a71898f190c45f
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-07-23 17:52:59 -0400

    fix

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "tfe_prereqs_w2" {
   tls_cert_fqdn    = local.tfe_fqdn
   tls_cert_sans = [
     local.tfe_pi_fqdn, local.tfe_pi_new_fqdn, local.tfe_irsa_new_fqdn,
-    local.tfe_pi_byo_fqdn, local.tfe_mixed, local.tfe_pi_byo_mixed
+    local.tfe_pi_byo_fqdn, local.tfe_mixed, local.tfe_byo_mixed
   ]
   tls_cert_email_address            = "nick.philbrook@hashicorp.com"
   tls_cert_route53_public_zone_name = local.r53_zone
