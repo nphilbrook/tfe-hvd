65d5f66fc88c1a2966027336f5f8597d4f6c2344
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-07-23 15:18:15 -0400

    upgraded user perms

--- user_perms.tf
+++ user_perms.tf
@@ -37,3 +37,104 @@ resource "aws_eks_access_policy_association" "tfe_pi_cluster_human" {
   policy_arn    = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
   principal_arn = local.it_me
 }
+
+resource "aws_eks_access_entry" "tfe_pi_new_cluster_human" {
+  cluster_name  = module.tfe_pi_new.eks_cluster_name
+  principal_arn = local.it_me
+  type          = "STANDARD"
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_access_policy_association" "tfe_pi_new_cluster_human" {
+  access_scope {
+    type       = "cluster"
+    namespaces = []
+  }
+
+  cluster_name = module.tfe_pi_new.eks_cluster_name
+
+  policy_arn    = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = local.it_me
+}
+
+resource "aws_eks_access_entry" "tfe_irsa_new_cluster_human" {
+  cluster_name  = module.tfe_irsa_new.eks_cluster_name
+  principal_arn = local.it_me
+  type          = "STANDARD"
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_access_policy_association" "tfe_irsa_new_cluster_human" {
+  access_scope {
+    type       = "cluster"
+    namespaces = []
+  }
+
+  cluster_name = module.tfe_irsa_new.eks_cluster_name
+
+  policy_arn    = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = local.it_me
+}
+
+resource "aws_eks_access_entry" "tfe_pi_byo_cluster_human" {
+  cluster_name  = module.tfe_pi_byo.eks_cluster_name
+  principal_arn = local.it_me
+  type          = "STANDARD"
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_access_policy_association" "tfe_pi_byo_cluster_human" {
+  access_scope {
+    type       = "cluster"
+    namespaces = []
+  }
+
+  cluster_name = module.tfe_pi_byo.eks_cluster_name
+
+  policy_arn    = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = local.it_me
+}
+
+resource "aws_eks_access_entry" "tfe_byo_mixed_cluster_human" {
+  cluster_name  = module.tfe_byo_mixed.eks_cluster_name
+  principal_arn = local.it_me
+  type          = "STANDARD"
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_access_policy_association" "tfe_byo_mixed_cluster_human" {
+  access_scope {
+    type       = "cluster"
+    namespaces = []
+  }
+
+  cluster_name = module.tfe_byo_mixed.eks_cluster_name
+
+  policy_arn    = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = local.it_me
+}
+
+resource "aws_eks_access_entry" "tfe_mixed_new_cluster_human" {
+  cluster_name  = module.tfe_mixed_new.eks_cluster_name
+  principal_arn = local.it_me
+  type          = "STANDARD"
+
+  tags = local.common_tags
+}
+
+resource "aws_eks_access_policy_association" "tfe_mixed_new_cluster_human" {
+  access_scope {
+    type       = "cluster"
+    namespaces = []
+  }
+
+  cluster_name = module.tfe_mixed_new.eks_cluster_name
+
+  policy_arn    = "arn:${data.aws_partition.current.partition}:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+  principal_arn = local.it_me
+}
+
