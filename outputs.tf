7f8c2ccc20b094be24a1e51fc8dd1e93fe7b8047
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-08-05 04:50:06 -0500

    new bastion output

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "bastion_public_dns" {
   description = "Public DNS name of bastion EC2 instance."
 }
 
+output "new_bastion_public_dns" {
+  value       = module.prereqs.bastion_public_dns
+  description = "Public DNS name of bastion EC2 instance."
+}
+
 output "gh_v4_hook_ranges" {
   value = local.gh_v4_hook_ranges
 }
