cd53141a22827eb28e65daa97f853419880fdd1a
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-08-29 15:57:55 -0400

    Create r53 record

--- locals.tf
+++ locals.tf
@@ -8,18 +8,18 @@ locals {
     "created-by"       = "terraform"
     "source_workspace" = var.TFC_WORKSPACE_SLUG
   }
-  
+
   # DEPRECATED
   tfe_fqdn          = "tfe.nick-philbrook.sbx.hashidemos.io"
   tfe_pi_fqdn       = "tfe-pi.nick-philbrook.sbx.hashidemos.io"
   tfe_irsa_new_fqdn = "tfe-irsa-new.nick-philbrook.sbx.hashidemos.io"
   tfe_pi_byo_fqdn   = "tfe-pi-byo.nick-philbrook.sbx.hashidemos.io"
-  tfe_byo_mixed = "tfe-pi-byo-mixed.nick-philbrook.sbx.hashidemos.io"
-  tfe_mixed     = "tfe-mixed.nick-philbrook.sbx.hashidemos.io"
+  tfe_byo_mixed     = "tfe-pi-byo-mixed.nick-philbrook.sbx.hashidemos.io"
+  tfe_mixed         = "tfe-mixed.nick-philbrook.sbx.hashidemos.io"
   # END DEPRECATED
 
   # CURRENT
-  tfe_pi_new_fqdn   = "tfe-pi-new.nick-philbrook.sbx.hashidemos.io"
+  tfe_pi_new_fqdn = "tfe-pi-new.nick-philbrook.sbx.hashidemos.io"
 
   it_me     = data.aws_iam_session_context.human.issuer_arn
   r53_zone  = "nick-philbrook.sbx.hashidemos.io"
