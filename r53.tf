74b016d8525c2d1aec87956a27ce98e4349355f0
Nick Philbrook <42616679+nphilbrook@users.noreply.github.com>
2025-08-30 15:04:40 -0400

    1) k8 YAML for an internal service, a network debugging pod, and an extra container to add an /etc/hosts override for the TFE hostname. 2) R53 zone and record for VPC-local hostname resolution to that internal service

--- r53.tf
+++ r53.tf
@@ -13,6 +13,7 @@ resource "aws_route53_record" "bastion" {
 data "aws_lb" "new" {
   tags = {
     "elbv2.k8s.aws/cluster" = module.tfe_new.eks_cluster_name
+    "service.k8s.aws/stack" = "tfe/terraform-enterprise"
   }
 }
 
@@ -22,4 +23,38 @@ resource "aws_route53_record" "tfe" {
   type    = "CNAME"
   ttl     = 300
   records = [data.aws_lb.new.dns_name]
+}
+
+# Create a private hosted zone for internal TFE resolution
+resource "aws_route53_zone" "tfe_internal" {
+  name = "nick-philbrook.sbx.hashidemos.io"
+
+  vpc {
+    vpc_id = module.prereqs.vpc_id
+  }
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name    = "${local.new_friendly_name_prefix}-tfe-internal-zone"
+      Purpose = "Internal DNS resolution for TFE"
+    }
+  )
+}
+
+# Data source to find the internal NLB by tags
+data "aws_lb" "internal" {
+  tags = {
+    "elbv2.k8s.aws/cluster" = module.tfe_new.eks_cluster_name
+    "service.k8s.aws/stack" = "tfe/terraform-enterprise-internal"
+  }
+}
+
+# Create a CNAME record that points to the internal NLB
+resource "aws_route53_record" "tfe_internal" {
+  zone_id = aws_route53_zone.tfe_internal.zone_id
+  name    = local.tfe_pi_new_fqdn
+  type    = "CNAME"
+  ttl     = 300
+  records = [data.aws_lb.internal.dns_name]
 }
